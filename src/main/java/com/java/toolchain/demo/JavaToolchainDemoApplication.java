ac5786622d2bdb6ec6fb1c85c777c658d0e9d419
mahabaleshwars <mahabaleshwar.s@infosys.com>
2024-04-26 15:07:08 +0530

    initial commit

---
+++ src/main/java/com/java/toolchain/demo/JavaToolchainDemoApplication.java
@@ -0,0 +1,13 @@
+package com.java.toolchain.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaToolchainDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaToolchainDemoApplication.class, args);
+	}
+
+}
