9c555327ccd7d05d57df29505b186e789b9f028a
Henrik Kaipe <kaipe>
2024-05-07 12:30:32 +0200

    Simplify project for faster build (and void functionality)

--- my_subdirectory/src/main/java/com/java/toolchain/demo/JavaToolchainDemoApplication.java
+++ my_subdirectory/src/main/java/com/java/toolchain/demo/JavaToolchainDemoApplication.java
@@ -1,13 +1,9 @@
 package com.java.toolchain.demo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class JavaToolchainDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JavaToolchainDemoApplication.class, args);
+		System.out.println("Hello World");
 	}
 
 }
--- my_subdirectory/src/test/java/com/java/toolchain/demo/JavaToolchainDemoApplicationTests.java
+++ my_subdirectory/src/test/java/com/java/toolchain/demo/JavaToolchainDemoApplicationTests.java
@@ -1,9 +1,7 @@
 package com.java.toolchain.demo;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class JavaToolchainDemoApplicationTests {
 
 	@Test
